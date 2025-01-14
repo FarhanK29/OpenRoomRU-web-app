a513037894dac9d782aa4ecc91ff5eb27fd6c19a
Farhan Khan <farhan.k0429@gmail.com>
2025-01-14 17:27:35 -0500

    updated frontend

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,27 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
+  ],
+})
 
 @Component({
   selector: 'app-root',
+  imports: [
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
-export class AppComponent {
-  roomCode: string = '';
-  dayOfWeek: string = '';
-  roomBookings: any[] = []; // This will hold the bookings data
 
-  getRoomSchedule() {
-    // Here you can make an HTTP request to the backend
-    // For now, I'll just mock the data.
-    this.roomBookings = [
-      {
-        start: new Date(), // Replace with actual start time
-        end: new Date(),   // Replace with actual end time
-        title: 'Math Class'
-      },
-      // Add more mocked events
-    ];
-  }
+export class AppComponent {
+  title = 'client';
 }
