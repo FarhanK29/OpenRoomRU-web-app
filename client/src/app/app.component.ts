fd40cdef6a4bf7303b1533c21f702988d132e116
Farhan Khan <farhan.k0429@gmail.com>
2025-01-17 10:41:02 -0500

    Rewrote client architecture

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgModule } from '@angular/core'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-
-
-@NgModule({
-  imports: [
-    BrowserAnimationsModule,
-    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
-  ],
-})
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-
 export class AppComponent {
   title = 'client';
 }
