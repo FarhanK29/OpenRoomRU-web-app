e55f88cb8a39b39d562a383c0b995b976f2146a8
Farhan Khan <farhan.k0429@gmail.com>
2025-01-17 16:43:11 -0500

    added imports

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 
+// Ng Materials Imports
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [
+    NavbarComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatAutocompleteModule,
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
